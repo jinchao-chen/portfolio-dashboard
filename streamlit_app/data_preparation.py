00cd69e8125ab9e0f9c67cf6a8d361e952c6503c
Jinchao <tjuchenjinchao@gmail.com>
2021-03-21 22:43:21 +0100

    include profit and return
     update pnl calculation using transaction records

--- streamlit_app/data_preparation.py
+++ streamlit_app/data_preparation.py
@@ -16,19 +16,6 @@ import altair as alt
 from datetime import datetime, timedelta
 
 
-# determine number of shares for each day
-def share_no_history(tr_pivoted, ticker):
-    """determine the share no of each ticker through pivoting the transaction history
-    """
-    tr_sub = tr_pivoted.loc[ticker].dropna(
-        subset=['buy', 'sell'], how='all').reset_index()
-    tr_sub = tr_sub.sort_values('Time')
-    tr_sub.rename({'Time': 'time_tr'}, axis=1, inplace=True)
-    tr_sub.drop(['Deposit', 'Dividend (Ordinary)',
-                 'Withdrawal'], axis=1, inplace=True)
-    return tr_sub
-
-
 def ticker_price_history(data, ticker):
     """filter time history of the specifed ticker, history of ticker close price """
     tts_sub = data[[('Close', ticker)]]  # ticker time series
@@ -45,40 +32,119 @@ def merge_histories(tr_sub, tts_sub, ticker):
     note: invested amount is determined based on the market price at close, rather than the transaction record
     """
     merged = tts_sub.merge(right=tr_sub, left_on='time_ts',
-                           right_on='time_tr', how="outer")
-    # , inplace=True, subset = ['buy', 'sell'], how = 'all')
-    merged[['buy', 'sell']] = merged[['buy', 'sell']].fillna(0)
-    merged['no_shares'] = (merged['buy']-merged['sell']).cumsum()
-    merged['invested'] = (merged['close_price'] *
-                          (merged['buy']-merged['sell'])).cumsum()
-    merged['values'] = merged['close_price']*merged['no_shares']
+                           right_on='Time', how="outer")
+    merged['cum_shares'] = merged['cum_shares'].fillna(method='ffill')
+    merged['value'] = merged['close_price']*merged['cum_shares']
     merged['ticker'] = ticker
+    merged['cum_total_eur'] = merged['cum_total_eur'].fillna(
+        method='ffill')
+    merged['value'] = merged['value'].fillna(
+        method='ffill')
     return merged
 
 
-"""import transaction data"""
+def calculate_average_price(group):
+    '''calculate the average price based on the invested value and floating number of shares'''
+
+    # cumulative total in euro (buy price*share - average_price*sell)
+    group['cum_total_eur'] = group['invested_eur'].cumsum()
+    group['ave_price_eur'] = group['cum_total_eur']/group['cum_shares']
+
+    return group
+
+
+def calculate_return(group):
+    """Update transaction time history, include calculation of average price
+    """
+
+    mask_buy = group["Action"] == 'buy'
+    mask_sell = group["Action"] == 'sell'
+
+    # determine the accumulated number of shares
+    group.loc[mask_buy, 'action_sign'] = 1
+    group.loc[mask_sell, 'action_sign'] = -1
+
+    group['cum_shares'] = (group['No. of shares'] *
+                           group['action_sign']).cumsum()
+
+    # average price, treating all actions as buy
+    group['pps_eur'] = group['Price / share'] / \
+        group['Exchange rate'].astype('float')  # price per share in eur
+    group['invested_eur'] = group['No. of shares']*group['pps_eur']
+    group = calculate_average_price(group)
+
+    # update the ave_price whenever a sell event occurs
+    for idx, row in group[mask_sell].iterrows():
+        group.loc[idx, 'invested_eur'] = -group.loc[idx,
+                                                    'No. of shares']*group.loc[idx-1, 'ave_price_eur']
+        group.loc[idx, 'ave_price_eur'] = group.loc[idx-1, 'ave_price_eur']
+        group = calculate_average_price(group)
+
+    # determine the return for each sell event
+    group.loc[mask_sell, 'profit_eur'] = group.loc[mask_sell, 'pps_eur']*group.loc[mask_sell, 'No. of shares'] + \
+        group.loc[mask_sell, 'invested_eur']
+
+    return group
+
+
+"""import and clean transaction data"""
 fln = "../data/trasaction_history_18022021.csv"
 tr = read_transactions(fln)
-tr_pivoted = tr.pivot_table(index=['Ticker', 'Time'],
-                            columns='action', values='No. of shares', dropna=False)
+
+# keep only the most relavent columns
+col_to_keep = ['Action', 'Time', 'Ticker', 'No. of shares', 'Price / share', 'Exchange rate',
+               'Result (EUR)']
+tr = tr[col_to_keep]
+tr.loc[tr['Action'].str.contains("buy"), 'Action'] = 'buy'
+tr.loc[tr['Action'].str.contains("sell"), 'Action'] = 'sell'
+# tr_pivoted = tr.pivot_table(index=['Ticker', 'Time'],
+#                             columns='action', values='No. of shares', dropna=False)
+
+tr = tr.loc[tr['Action'].isin(['buy', 'sell'])]
+grouped = tr.groupby(by='Ticker')
+
+# feature engineering
+groups = []
+
+for name, group in grouped:
+    group.reset_index(inplace=True, drop=True)
+    group = calculate_return(group)
+    groups.append(group)
+
+tr = pd.concat(groups).reset_index(drop=True)
 
 """request time history from yahoo finance"""
+
+# download the ts
 tickers = tr.Ticker.dropna().unique()
 tickers = tickers.tolist()
-
 start = tr.Time.min()
 end = tr.Time.max() + timedelta(1)
-
 data = yf.download(tickers, start, end)
-# loop it through for all the tickers:
 
+#  identify tickers not downloaded
+ts_tickers = data.columns.droplevel(0).values
+mask = data['Adj Close'].isna().mean() == 1.0
+tickers_to_drop = mask.loc[mask].keys().values
+
+# loop it through for all the tickers:
+# loop it through for all the tickers:
 dfs = []
+groups = tr.groupby(by='Ticker')
+ts_tickers = data.columns.droplevel(0).values
 
 for ticker in tickers:
-    tr_sub = share_no_history(tr_pivoted, ticker)
-    tts_sub = ticker_price_history(data, ticker)
-    df = merge_histories(tr_sub, tts_sub, ticker)
+    if ticker not in tickers_to_drop:
+        # share_no_history(tr_pivoted,ticker)
+        tr_sub = groups.get_group(
+            ticker)[['Time', 'Ticker', 'cum_shares', 'cum_total_eur', 'profit_eur']]
+        tts_sub = ticker_price_history(data, ticker)
+        df = merge_histories(tr_sub, tts_sub, ticker)
 
-    dfs.append(df)
+        dfs.append(df)
+    else:
+        print(f'{ticker} not in the database')
 
 df_combined = pd.concat(dfs)
+df_combined = df_combined.drop_duplicates(
+    subset=['time_ts', 'ticker'], keep='last', inplace=False, ignore_index=False)
--- streamlit_app/web_app.py
+++ streamlit_app/web_app.py
@@ -7,56 +7,131 @@ from datetime import datetime, timedelta
 from datetime import date, timedelta
 import plotly.graph_objs as go
 import plotly.express as px
-from data_preparation import df_combined, tr, end
+from data_preparation import df_combined, tr, start, end, data
+import plotly.io as pio
 
-st.title('Stock dashboard')
+pio.templates.default = 'plotly_white'
+st.set_page_config(layout="wide")
 
-# mothly transaction overview
-mt = (
-    tr.groupby(by=[pd.Grouper(key="Time", freq="M"), "action"])["action"]
-    .count()
-    .rename("transactions")
-    .reset_index()
-)  # montly transaction
+# row0
+row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.beta_columns(
+    (.1, 2, .2, 1, .1))
+row0_1.title('A dashboard for stock tradings')
 
-fig = go.Figure()
+with row0_2:
+    st.write('')
 
-# reformat the date_time to provide a monthly summary (do not show the date)
-mt['mnth_yr'] = mt['Time'].apply(lambda x: x.strftime('%b-%Y'))
+# row1
+st.write('')
+row1_space1, row1_1, row1_space2, row1_2, row1_space3 = st.beta_columns(
+    (.1, 1, .1, 1, .1))
+# line plot for portofolio time history
+with row1_1:
+    df_agg = df_combined.pivot_table(index='time_ts', values=[
+                                 'value', 'cum_total_eur', 'profit_eur'], aggfunc='sum').reset_index()
+    df_agg['realized_profit'] = df_agg['profit_eur'].cumsum()
+    df_agg['floating_profit'] = df_agg['value'] - df_agg['cum_total_eur']
 
-for action in ['buy', 'sell']:
-    mt_ = mt.loc[mt['action'] == action]
-    fig.add_trace(go.Bar(
-        x=mt_.mnth_yr,
-        y=mt_.transactions,
-        name=action,
-    ))
+    df = px.data.stocks()
 
-fig.update_layout(barmode='group', xaxis_tickangle=-45, title_text='monthly transactions overview',
-                  yaxis=dict(
-                      title='transaction counts',
-                  ))
-st.plotly_chart(fig)
+    df = px.data.stocks()
+
+    fig11 = px.line(df_agg, x="time_ts", y=['value','cum_total_eur'],
+                hover_data={"time_ts": "|%B %d, %Y"},
+                title='Overview'
+                )
+
+    fig11.update_xaxes(
+        dtick="M1",
+        tickformat="%b\n%Y")
+
+    fig11.update_layout(hovermode="x")
+    st.plotly_chart(fig11)
+
+with row1_2:
+    st.write('')
+    st.subheader('A brief summary')
+    st.markdown("You started investing with Trading212 on {:}.".format(
+        start.strftime("%b-%d-%Y")))
+    st.markdown("Upon {:}, you've invested {:} euro in the market, with a floating profit of {:} euro. Total realized profit amounts {:} euro".format(end.strftime("%b-%d-%Y"), 213, 22, 23))
+
+# row2
+st.write('')
+row2_space1, row2_1, row2_space2, row2_2, row2_space3 = st.beta_columns(
+    (.1, 1, .1, 1, .1))
+
+# fig_1-1, mothly transaction overview
+with row2_1:
+    mt = (
+        tr.groupby(by=[pd.Grouper(key="Time", freq="M"), "Action"])["Action"]
+        .count()
+        .rename("transactions")
+        .reset_index()
+    )  # montly transaction
+
+    fig21 = go.Figure()
+    mt['mnth_yr'] = mt['Time'].apply(lambda x: x.strftime('%b-%Y'))
+
+    for action in ['buy', 'sell']:
+        mt_ = mt.loc[mt['Action'] == action]
+        fig21.add_trace(go.Bar(
+            x=mt_.mnth_yr,
+            y=mt_.transactions,
+            name=action,
+        ))
+
+    fig21.update_layout(template='plotly_white',
+                        barmode='group', xaxis_tickangle=-45, title_text='monthly transactions overview',
+                        yaxis=dict(
+                            title='transaction counts',
+                        )
+                        )
+    st.plotly_chart(fig21)
 
 # pie chart of portofolio composition
-df_ = df_combined.loc[(df_combined['time_ts'] == end -
-                       timedelta(1)) & (df_combined['no_shares'] > 0.25)]
+with row2_2:
+    df_ = df_combined.loc[(df_combined['time_ts'] == end -
+                          timedelta(1)) & (df_combined['cum_shares'] > 0.25)]
+    df_ = df_.drop_duplicates(
+    subset=['time_ts', 'Ticker'], keep='last', inplace=False, ignore_index=False)
 
-fig = px.pie(df_, values='values', names='ticker')
-st.plotly_chart(fig)
+    fig22 = px.pie(df_, values='value', names='Ticker',
+                   title='portofolio composition')
+    st.plotly_chart(fig22)
+    # fig22.update_layout(title='portofolio composition')
 
-# line plot for portofolio time history
-df_ = df_combined.pivot_table(index='time_ts', values=[
-                                 'values', 'invested'], aggfunc='sum').reset_index()
-df = px.data.stocks()
+# row3
+st.write('')
+row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.beta_columns(
+    (.1, 1, .1, 1, .1))
 
-fig = px.line(df_, x="time_ts", y= ['values','invested'],
-              hover_data={"time_ts": "|%B %d, %Y"},
-              title='Overview'
-             )
+with row3_1:
+    retscomp = data['Adj Close'].pct_change()
+    corr = retscomp.corr()
+    cols = df_.Ticker.dropna().unique()
+    retscomp = data.loc[:, ('Adj Close', cols)].droplevel(0, axis =1)
+    retscomp = retscomp.pct_change()
+    corr = retscomp.corr()
+    fig31 = go.Figure(data=go.Heatmap(
+        z=corr,
+        x=cols,
+        y=cols,
+        colorscale='Hot',
+        reversescale=True,
+        zmax=1.0,
+        zmin=0.0))
 
-fig.update_xaxes(
-    dtick="M1",
-    tickformat="%b\n%Y")
+    fig31.update_layout(title_text='Stock correlation analysis',)
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig31)
+
+with row3_2:
+    fig32 = px.scatter(x=retscomp.mean(), y=retscomp.std(), text=cols)
+    fig32.update_traces(textposition='top center')
+    fig32.update_layout(
+    #     height=800,
+        title_text='Return and Risk',
+        xaxis_title="Return",
+        yaxis_title="Risk",
+    )
+    st.plotly_chart(fig32)
